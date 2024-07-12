62edb0f5c5471d4f6da65b89eaf7dc355488b84a
bagusaff <fkrfajri@gmail.com>
2024-07-12 09:20:56 +0700

    Update RN 0.74.3 & Newest dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,46 +1,45 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins:[
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
     [
-      "formatjs",
+      'formatjs',
       {
-        "idInterpolationPattern": "[sha512:contenthash:base64:6]",
-        "ast": true
-      }
+        idInterpolationPattern: '[sha512:contenthash:base64:6]',
+        ast: true,
+      },
     ],
     [
-      "module-resolver",
+      'module-resolver',
       {
-          "root": ["./src"],
-          "extensions":[
-              ".js",
-              ".ts",
-              ".jsx",
-              ".tsx",
-              ".ios.tsx",
-              ".android.tsx",
-              ".ios.ts",
-              ".android.ts",
-              ".ios.js",
-              ".android.js",
-              ".ios.jsx",
-              ".android.jsx"
-          ],
-          "alias":{
-              "components":"./src/components",
-              "styles":"./src/styles",
-              "assets":"./src/assets",
-              "screens":"./src/screens",
-              "navigation":"./src/navigation",
-              "stacks":"./src/stacks",
-              "store":"./src/store",
-              "utils":"./src/utils",
-              "theme":"./src/theme",
-              "types":"./src/types",
-          }
-      }
+        root: ['./src'],
+        extensions: [
+          '.js',
+          '.ts',
+          '.jsx',
+          '.tsx',
+          '.ios.tsx',
+          '.android.tsx',
+          '.ios.ts',
+          '.android.ts',
+          '.ios.js',
+          '.android.js',
+          '.ios.jsx',
+          '.android.jsx',
+        ],
+        alias: {
+          components: './src/components',
+          styles: './src/styles',
+          assets: './src/assets',
+          screens: './src/screens',
+          navigation: './src/navigation',
+          stacks: './src/stacks',
+          store: './src/store',
+          utils: './src/utils',
+          theme: './src/theme',
+          types: './src/types',
+        },
+      },
     ],
     'react-native-reanimated/plugin', // needs to be last
-
-  ]
+  ],
 };
